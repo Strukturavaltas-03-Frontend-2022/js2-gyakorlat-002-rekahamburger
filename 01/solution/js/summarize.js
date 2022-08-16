c2d42a064da935c6bdfa802d063add476bfc6a85
Hamburger Réka <Rka>
2022-08-16 18:40:40 +0200

    js2 gyakorlat

--- 01/solution/js/summarize.js
+++ 01/solution/js/summarize.js
@@ -3,5 +3,29 @@
 
 
 
+function summarize() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    if (typeof (arguments[i]) === "number") {
+      if (Number.isSafeInteger(arguments[i])) {
+        sum = sum + arguments[i];
+      }
+      else {
+        const bigIntNumber = BigInt(arguments[i]);
+        sum = BigInt(sum)
+        sum = sum + bigIntNumber;
+      }
+    }
+  }
+  return sum;
 
-// export default summarize;
+}
+
+
+
+
+
+
+export default summarize;
+
+
---
+++ 02/solution/js/numericConverter.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+function numericConverter() {
+    
+            const hexa = 0x1F;
+            const binary = 0b11111;
+            const octal = 0o37;
+
+            console.log([ hexa, bin, octal]);
+    
+}
